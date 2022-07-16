0bee0614686b387a08d2707cdf81fa4df37023df
Nebiyyeh Dagdeviren <nebiyyehdagdeviren@gmail.com>
2022-07-16 14:29:26 -0500

    Second Commit

--- src/MyGitClass.java
+++ src/MyGitClass.java
@@ -1,6 +1,8 @@
 public class MyGitClass {
     public static void main(String[] args) {
         System.out.println("Version 1");
+
+        System.out.println("I am learning github!!");
     }
 
 }
