f58b294d00f2e010cd6f87466be70fd368cb4330
Nebiyyeh Dagdeviren <nebiyyehdagdeviren@gmail.com>
2022-07-16 14:50:25 -0500

    Third commit

--- src/MyGitClass.java
+++ src/MyGitClass.java
@@ -3,6 +3,8 @@ public class MyGitClass {
         System.out.println("Version 1");
 
         System.out.println("I am learning github!!");
+
+        System.out.println("This is the third commit!");
     }
 
 }
